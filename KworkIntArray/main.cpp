2f668502985cb91a0131c28e94c9f89c33d7c87a
Maxim <max77648@gmail.com>
2021-11-17 05:31:29 +0300

    Доделал все методы

--- KworkIntArray/main.cpp
+++ KworkIntArray/main.cpp
@@ -9,13 +9,14 @@ private:
 	T* _array;
 	size_t _size;
 
-	IntArray(T* arr, size_t size)
+	IntArray(T*& arr, size_t size)
 	{
 		_size = size;
 		_array = new T[_size];
 		for(size_t i = 0; i < _size; i++)
 		{
 			_array[i] = arr[i];
+			std::cout << arr[i] << endl;
 		}
 	}
 public:
@@ -35,9 +36,8 @@ public:
 		}
 	}
 
-	IntArray(const IntArray& arr)
+	IntArray(const IntArray& arr): _array(nullptr), _size(arr._size)
 	{
-		_size = arr._size;
 		_array = new T[_size];
 		for(size_t i = 0; i < _size; i++)
 		{
@@ -45,12 +45,37 @@ public:
 		}
 	}
 
+	IntArray& operator=(const IntArray& arr)
+	{
+		if(_array)
+			delete[] _array;
+
+		_size = arr._size;
+		_array = new T[_size];
+		memcpy(_array, arr._array, _size);
+		return *this;
+	}
+
+	IntArray(IntArray&& arr): _array(arr._array), _size(arr._size)
+	{
+		arr._array = nullptr;
+	}
+
+	IntArray& operator=(IntArray&& arr)
+	{
+		if(_array)
+			delete[] _array;
+
+		_size = arr._size;
+		_array = arr._array;
+		arr.pBuff = nullptr;
+		return *this;
+	}
+
 	~IntArray()
 	{
 		delete[] _array;
-		_size = 0;
 	}
-
 
 	size_t GetSize()
 	{
@@ -105,7 +130,7 @@ public:
 		_array = newArray;
 	}
 
-	IntArray operator + (IntArray arr)
+	IntArray operator + (IntArray& arr)
 	{
 		if(arr._size != _size && _size != NULL)
 		{
@@ -120,6 +145,101 @@ public:
 
 		return IntArray(newArray, _size);
 	}
+
+	IntArray operator += (IntArray& arr)
+	{
+		if(arr._size != _size && _size != NULL)
+		{
+			throw std::invalid_argument("Sizes must be equals!");
+		}
+
+		for(size_t i = 0; i < _size; i++)
+		{
+			_array[i] += arr._array[i];
+		}
+
+		return *this;
+	}
+
+	IntArray operator * (size_t size)
+	{
+		if(size == 0 || size == 1)
+		{
+			return *this;
+		}
+
+		size_t newSize = _size * size;
+		IntArray newArray(newSize);
+		size_t index = 0;
+		for(size_t i = 0; i < size; i++)
+		{
+			for(size_t j = 0; j < _size; j++)
+			{
+				newArray._array[j + i] = _array[j];
+			}
+		}
+
+		return newArray;
+	}
+
+	IntArray operator *= (size_t size)
+	{
+		if(size == 0 || size == 1)
+		{
+			return *this;
+		}
+
+		size_t newSize = _size * size;
+		T* newArray = new T[newSize];
+		size_t index = 0;
+		for(size_t i = 0; i < size; i++)
+		{
+			for(size_t j = 0; j < _size; j++)
+			{
+				newArray[j + i] = _array[j];
+			}
+		}
+
+		delete[] _array;
+		_array = newArray;
+		_size = newSize;
+
+		return *this;
+	}
+
+	void operator () (T row)
+	{
+		for(size_t i = 0; i < _size; i++)
+		{
+			_array[i] += row;
+		}
+	}
+
+	const int operator[] (const size_t index) const
+	{
+		return _array[index];
+	}
+
+	friend std::istream& operator >> (std::istream& in, IntArray& arr)
+	{
+		in >> arr._size;
+		arr._array = new T[arr._size];
+		for(size_t i = 0; i < arr._size; i++)
+		{
+			in >> arr._array[i];
+		}
+		return in;
+	}
+
+	friend std::ostream& operator << (std::ostream& os, const IntArray& arr)
+	{
+		for(size_t i = 0; i < arr._size - 1; i++)
+		{
+			os << arr._array[i] << ' ';
+		}
+		os << arr._array[arr._size - 1];
+		return os;
+	}
 };
 
 int main()
@@ -127,7 +247,7 @@ int main()
 	try
 	{
 		size_t size = 10;
-		IntArray<int> a(size);
+		/*IntArray<int> a(size);
 
 		std::cout << "pop" << endl;
 		for(size_t i = 0; i < size; i++)
@@ -142,15 +262,20 @@ int main()
 		{
 			a.Append(i);
 			std::cout << a.GetSize() << endl;
-		}
-	
+		}*/
+
 		IntArray<int> b;
-		IntArray<int> c;
+		//IntArray<int> c;
 
 		b.Append(5);
-		c.Append(5);
 
-		b + c;
+		std::cout << b << endl;
+
+		b *= 100;
+		std::cout << b << endl;
+
+		std::cout << b * 2 << endl;;
+
 	}
 	catch(const std::invalid_argument& err)
 	{
